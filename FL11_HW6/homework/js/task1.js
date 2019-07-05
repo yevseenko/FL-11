66e55b6aae9cfdfb83ad1fb2197e63d50a4909b0
yevseenko <yuriy.evseenko@gmail.com>
2019-07-05 13:09:37 +0300

    first task

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,41 +1,21 @@
-const coordinates = {
-  'A': {
-    'x': 0,
-    'y': 0
-  },
-  'B': {
-    'x': 0,
-    'y': 0
-  },
-  'C': {
-    'x': 0,
-    'y': 0
+let pointNames = ['aX', 'aY', 'bX', 'bY', 'cX', 'cY'];
+let points = {};
+
+for (let i = 0; i < pointNames.length; i++) {
+  const val = +prompt(`Enter coordinate of ${pointNames[i]}:`, '');
+  if (isNaN(val) || !isFinite(val)) {
+    points = null;
+    alert(`[${val}] - not a number, please press F5 to continue`);
+    break;
+  } else {
+    points[pointNames[i]] = val;
   }
 }
 
-let isNumber = true;
-
-for (let key in coordinates) {
-  if (coordinates[key].hasOwnProperty('x') && coordinates[key].hasOwnProperty('y')) {
-    coordinates[key]['x'] = +prompt(`Enter coordinate X of point ${key}:`, '');
-    if (isNaN(coordinates[key]['x']) || !isFinite(coordinates[key]['x'])) {
-      isNumber = false;
-      alert(`[${coordinates[key]['x']}] - not a number, please press F5 to continue`);
-      break;
-    }
-    coordinates[key]['y'] = +prompt(`Enter coordinate Y of point ${key}:`, '');
-    if (isNaN(coordinates[key]['y']) || !isFinite(coordinates[key]['y'])) {
-      isNumber = false;
-      alert(`[${coordinates[key]['y']}] - not a number, please press F5 to continue`);
-      break;
-    }
-  }
-}
-
-if (isNumber) {
+if (points) {
   let magicNumber = 2;
-  let halfOnX = (coordinates['A']['x'] + coordinates['B']['x']) / magicNumber === coordinates['C']['x'];
-  let halfOnY = (coordinates['A']['y'] + coordinates['B']['y']) / magicNumber === coordinates['C']['y'];
+  let halfOnX = (points['aX'] + points['bX']) / magicNumber === points['cX'];
+  let halfOnY = (points['aY'] + points['bY']) / magicNumber === points['cY'];
 
   halfOnX && halfOnY ? console.log(true) : console.log(false);
 }
